8ee9a4924912b8bb8f43a198def94b27af1fc86b
ujjwalpokharel <ujjwalppokharel@gmail.com>
2025-10-22 09:22:51 +0545

    Rebuild for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/client-website",
+  base:"/client-website/",
   build: {
     rollupOptions: {
       output: {
